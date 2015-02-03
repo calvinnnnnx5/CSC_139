acf92ca135b89f2cda34e65fa86b557cece3ae62
Konstantin Polyashenko <kman239@gmail.com>
2015-02-03 15:30:10 -0800

    starting proj1

---
+++ project1/prog1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <string.h>
+
+//multiprocessed C program to do matric multiplication 
+//	of two integer matrixed
+int main(int argc, char *argv[]) 
+{
+	
+}
